2015b52bea4085aca0dc370ffcc0f54c78510ce6
everyoon <jy.k7377@gmail.com>
2025-02-24 15:34:58 +0900

    sass 파일명 .css로 변경

--- js/main.js
+++ js/main.js
@@ -43,41 +43,32 @@ document.addEventListener('DOMContentLoaded', function () {
     },
   });
 
-  let isScrolling = false; // 중복 스크롤 방지 변수
-
-  // .title과 section 요소들 모두 포함
+  // 마우스 스크롤 이벤
+  let isScrolling = false;
   const sections = document.querySelectorAll('.title, .about, .project, .contact');
-  let currentIndex = 0; // 현재 섹션의 인덱스
+  let currentIndex = 0;
 
-  // 섹션이 제대로 선택되었는지 확인
   if (sections.length === 0) {
     console.error('섹션을 찾을 수 없습니다!');
     return; // 섹션을 찾을 수 없으면 함수 종료
   }
-
   // 스크롤 이벤트 처리
   window.addEventListener('wheel', function (event) {
-    if (isScrolling) return; // 이미 스크롤 중이면 무시
+    if (isScrolling) return;
 
-    isScrolling = true; // 스크롤 시작
+    isScrolling = true;
 
-    // 아래로 스크롤 시
     if (event.deltaY > 0) {
       currentIndex = Math.min(currentIndex + 1, sections.length - 1);
-    }
-    // 위로 스크롤 시
-    else {
+    } else {
       currentIndex = Math.max(currentIndex - 1, 0);
     }
 
-    // 해당 섹션으로 부드럽게 이동
     if (sections[currentIndex]) {
       sections[currentIndex].scrollIntoView({ behavior: 'smooth' });
     }
-
-    // 스크롤이 끝난 후 잠시 대기
     setTimeout(() => {
       isScrolling = false;
-    }, 500); // 800ms 후 스크롤 이벤트 가능
+    }, 500);
   });
 });
